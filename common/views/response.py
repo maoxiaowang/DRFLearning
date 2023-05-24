26346e719c0b131b90cedc73795bcdd509b7b36a
Oliver <oliver@aidriven.com.au>
2023-05-24 17:11:33 +0800

    Response优化

--- common/views/response.py
+++ common/views/response.py
@@ -1,6 +1,8 @@
 from typing import Union
 
 from django.conf import settings
+from django.core.files import File
+from django.core.files.images import ImageFile
 from rest_framework import status as http_status
 from rest_framework.response import Response as _Response
 
@@ -15,11 +17,21 @@ class Response(_Response):
             request = context['request']
             user = request.user
             wsgi_request = getattr(request, '_request')
+            debug_data = request.data
+            files: dict = getattr(request, '_files', {})
+            file_fields = [field for field, value in files.items()] if files else list()
+            for field in file_fields:
+                value = debug_data[field]
+                if isinstance(value, (File, ImageFile)):
+                    debug_data[field] = value.name
+                else:
+                    debug_data[field] = 'Unknown file'
+
             debug_info = {
                 'host': getattr(wsgi_request, '_current_scheme_host'),
                 'path': wsgi_request.path,
                 'method': wsgi_request.method,
-                'data': request.data,
+                'data': debug_data,
                 'user': {
                     'id': user.id if user.is_authenticated else None,
                     'username': user.username if user.is_authenticated else str(user)
